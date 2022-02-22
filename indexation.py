8601c460ef01899ef56d04a6a632bcdf0a041ecd
Sofiane Zeghoud <sofianezegood@gmail.com>
2022-02-22 12:19:57 +0100

    few code tweaks

--- indexation.py
+++
@@ -1,8 +0,0 @@
-from face_recognition import face_distance
-import numpy as np
-
-a = np.array([1 for i in range(128)])
-b = np.array([0 for i in range(128)])
-print(a.shape)
-distance = face_distance(a, b)
-print(distance)
